800b816b9e1c4a65c6820fba89a36474c8b17014
Pavel Penchev <pavel.penchev@gmail.com>
2017-04-19 06:16:49 +0300

    Text changes, limit the chars we write to the file

--- notify.php
+++ notify.php
@@ -1,5 +1,8 @@
 <?php
 $email = $_REQUEST["notify-email"];
-file_put_contents('/var/www/rebulas-notify/notify.txt', $email.PHP_EOL , FILE_APPEND | LOCK_EX);
+
+if (strlen($email) < 40) {
+  file_put_contents('/var/www/rebulas-notify/notify.txt', $email.PHP_EOL , FILE_APPEND | LOCK_EX);
+}
 ?>
 OK
