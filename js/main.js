5f85093d3c86e25e8bc79a04fc41ef7bc5952608
Pavel Penchev <pavel.penchev@gmail.com>
2017-10-11 12:42:41 +0300

    Drop unused js

--- js/main.js
+++
@@ -1,7 +0,0 @@
-function notify() {
-  var email = $("#notify-email").val();
-  $.post( "notify.php", {"notify-email" : email}, function( data ) {
-    $( "#notify-form" ).hide();
-    $( "#thank-you" ).show();
-  });
-}
--- notify.php
+++
@@ -1,8 +0,0 @@
-<?php
-$email = $_REQUEST["notify-email"];
-
-if (strlen($email) < 40) {
-  file_put_contents('/var/www/rebulas-notify/notify.txt', $email.PHP_EOL , FILE_APPEND | LOCK_EX);
-}
-?>
-OK
